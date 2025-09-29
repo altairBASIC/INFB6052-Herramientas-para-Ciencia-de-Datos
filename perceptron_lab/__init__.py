0550174c103fec770ecc8a0d367064c712548b4c
Ignacio <ignacio@example.com>
2025-09-29 19:32:42 -0300

    Mantener solo semana-01-entorno (untrack otras carpetas)

--- perceptron_lab/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Paquete raíz perceptron_lab.
-Exponer clase PerceptronScratch/Perceptron.
-"""
-from .perceptron.perceptron import PerceptronScratch, Perceptron
-__all__ = ["PerceptronScratch", "Perceptron"]
